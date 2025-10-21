9edd39c093a5565d0a04ff8418c2b9a48a529491
pokalapavan2004 <169077637+pokalapavan2004@users.noreply.github.com>
2025-10-21 23:05:02 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get('DATABASE_URL')
-        or 'postgresql+psycopg2://postgres:admin123@localhost:5432/flipkart_support'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
